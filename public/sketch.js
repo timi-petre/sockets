c5b4bfeef9d9318dc381a5104dcdccb75c441baa
Timotei Petre <gaxxis@yahoo.com>
2022-10-25 11:59:22 +0300

    Reset btn

--- public/sketch.js
+++ public/sketch.js
@@ -1,17 +1,51 @@
 let socket;
-
+let cnv;
 function setup() {
-    createCanvas(600, 400);
-    background(51);
+    cnv = createCanvas(900, 600);
+    if (navigator.userAgent.indexOf('Win') != -1) {
+        const button = createButton('reset');
+        let val = random(255);
+        let text = createP('Please draw something on Windows');
 
+        text.class('stil');
+        button.class('btn-reset');
+        background(val);
+
+        button.position(900, 150);
+        text.position(730, 50);
+
+        cnv.position(500, 250);
+        button.mousePressed(resetSketch);
+    }
+
+    if (navigator.userAgent.indexOf('Android') != -1) {
+        text = createP('Please draw something on Android');
+
+        text.class('stil');
+        background(51, 24, 662);
+
+        text.position(250, 20);
+        cnv.position(50, 150);
+    }
     socket = io.connect('http://192.168.1.236:3000');
     socket.on('mouse', newDrawing);
 }
+
 function newDrawing(data) {
     noStroke();
     fill(255, 0, 100);
     ellipse(data.x, data.y, 30, 30);
 }
+function resetSketch() {
+    let val = random(255);
+    console.log('Button pressed: ' + val);
+    background(val);
+    const nodata = {
+        pressed: val,
+    };
+    socket.emit('mouse', nodata);
+}
+
 function mouseDragged() {
     console.log('Sending: ' + mouseX + ',' + mouseY);
 
@@ -19,6 +53,7 @@ function mouseDragged() {
         x: mouseX,
         y: mouseY,
     };
+
     socket.emit('mouse', data);
 
     noStroke();
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 const server = app.listen(3000);
@@ -7,19 +8,22 @@ app.use(express.static('public'));
 
 console.log('My socket server is running on port 3000');
 
-const socket = require('socket.io');
-
 const io = socket(server);
 
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
     console.log('new connection' + socket.id);
-
     socket.on('mouse', mouseMsg);
+    socket.on('mouse', resetMsg);
+
     function mouseMsg(data) {
         socket.broadcast.emit('mouse', data);
         //io.sockets.emit('mouse',data);
         console.log(data);
+    }
+    function resetMsg(nodata) {
+        socket.broadcast.emit('mouse', nodata);
+        console.log(nodata);
     }
 }
